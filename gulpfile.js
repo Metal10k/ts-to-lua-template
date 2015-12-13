2a5c4b510c176b39ad164ffd43f5477bba125bf3
Metal10k <Metal10k@hotmail.com>
2015-12-13 16:44:06 +0000

    Simple working

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var shell = require("gulp-shell");
+var clean = require('gulp-clean');
+var glob = require("glob");
+var gulpsync = require('gulp-sync')(gulp);
+//var colony = require("js-to-lua");
+
+gulp.task("test", function(){
+	var stuff = glob.sync("src/**/*.js");
+	console.log(stuff);
+});
+
+var compilerPath = "node node_modules/js-to-lua/colony.js";
+
+
+gulp.task('clean-js', function () {
+	return gulp.src('src/**/*.js', {read: false})
+		.pipe(clean());
+});
+
+gulp.task('clean-lua', function () {
+	return gulp.src('src/**/*.lua', {read: false})
+		.pipe(clean());
+});
+
+gulp.task("ts", function(){
+	return gulp.src('src/**/*.ts')
+    	.pipe(ts({
+		module: 'commonjs',
+		target: 'ES5'
+		}))
+    	.pipe(gulp.dest('src'));
+});
+
+gulp.task("lua", shell.task(
+	glob.sync("src/**/*.js").map(function(path){
+		return compilerPath + " " + path;// + " " + path.replace("src/", "bin/");
+	})
+	));
+
+gulp.task("clean", ["clean-lua", "clean-js"]);
+
+gulp.task("default", gulpsync.sync(['clean', 'ts', 'lua']));
---
+++ src/something.ts
@@ -0,0 +1,5 @@
+function test(){
+	console.log("I am a function");
+}
+
+export = test;
